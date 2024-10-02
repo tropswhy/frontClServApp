8711139311235ae5ef6e22c309544b944d54113b
tropswhy <solodkovnikita08@gmail.com>
2024-10-02 21:27:04 +0300

    добавил json в contractStore

--- src/stores/ContractStore.ts
+++ src/stores/ContractStore.ts
@@ -1,5 +1,7 @@
 import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import contractBD from '../../back/contracts.json'
 
 export const useContractStore = defineStore('contractStore', () => {
-    
+    const contracts = ref <object>(contractBD)
 })
