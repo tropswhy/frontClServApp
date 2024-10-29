61987d149c83e08d92a8f7348c21bc32d6af31e5
tropswhy <solodkovnikita08@gmail.com>
2024-10-29 19:46:40 +0300

    добавил в связи mid icons

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createVuetify } from 'vuetify'
 import 'vuetify/styles'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-//import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 import router from './router'
 
@@ -12,7 +12,7 @@ const app = createApp(App)
 const vuetify = createVuetify({
   components,
   directives,
-  //iconfont: 'mdi',  
+  iconfont: 'mdi',  
 })
 
 app.use(createPinia())
